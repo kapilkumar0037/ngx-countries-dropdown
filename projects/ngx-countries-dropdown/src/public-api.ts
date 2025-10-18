194c056c44a4b0a6ed2f0102b2a658e03df1927f
Kapil Kumar <kapilkumar0037@gmail.com>
2025-10-18 21:47:41 +0530

    country list exported

--- projects/ngx-countries-dropdown/src/public-api.ts
+++ projects/ngx-countries-dropdown/src/public-api.ts
@@ -5,3 +5,4 @@ export { CountryListComponent } from './lib/components/country-list/country-list
 export { CountryListStandaloneComponent } from './lib/components/country-list-standalone/country-list-standalone.component';
 export { IConfig, ICountry } from './lib/models';
 export * from './lib/ngx-countries-dropdown.module';
+export { COUNTRIES_LIST } from './lib/constants';
